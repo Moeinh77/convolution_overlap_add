2b84d6e60bcf3860276cfcb9bbeaffa8a83b5638
mesquita <vinicius@outlook.com>
2019-07-14 10:32:42 -0300

    Organizing example timing

--- example_timing.py
+++ example_timing.py
@@ -1,8 +1,11 @@
 from utils.timing import lets_time_it, time_pure_numpy
 
-# lets_time_it(which='toeplitz')
+# Comparing the different modes
+lets_time_it(which='toeplitz')
+lets_time_it(which='scipy_auto')
+lets_time_it(which='scipy_direct')
+lets_time_it(which='scipy_fft')
+
+# Comparing pure numpy with our implementation
 lets_time_it(which='numpy_convolve')
 time_pure_numpy()
-# lets_time_it(which='scipy_auto')
-# lets_time_it(which='scipy_direct')
-# lets_time_it(which='scipy_fft')
